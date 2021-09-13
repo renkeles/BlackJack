aba93c24030d8fa1f4af76c0ac604e8369b7b5d5
renkeles <84261162+renkeles@users.noreply.github.com>
2021-09-13 16:25:00 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
+#include <vector>
 #include <iostream>
+#include <string>
 
+int main()
+{
+    std::cout << "\t\tWelcome to Blackjack!\n\n";
 
+    int numPlayers = 0;
+    while (numPlayers < 1 || numPlayers > 7)
+    {
+        std::cout << "How many players? (1 - 7): ";
+        std::cin >> numPlayers;
+    }
 
-int main(){
+    std::vector<std::string> names;
+    std::string name;
+    for (int i = 0; i < numPlayers; ++i)
+    {
+        std::cout << "Enter player name: ";
+        std::cin >> name;
+        names.push_back(name);
+    }
+    std::cout << std::endl;
 
-
-	return 0;
+    // игровой цикл
+    /*
+    Game aGame(names);
+    char again = 'y';
+    while (again != 'n' && again != 'N')
+    {
+        aGame.Play();
+        std::cout << "\nDo you want to play again? (Y/N): ";
+        std::cin >> again;
+    }
+    */
+    return 0;
 }
