ffa79c469d67000709938724112927afc9c27c06
renkeles <84261162+renkeles@users.noreply.github.com>
2021-09-16 14:08:54 +0500

    Update GenericPlayer.h

--- GenericPlayer.h
+++ GenericPlayer.h
@@ -1,28 +1,74 @@
 #pragma once
+#include <iostream>
 #include "Hand.h"
 #include <string>
-#include <iostream>
+#include <vector>
 
-class GenericPlayer : public Hand {
-private:
-	std::string m_Name; // Имя игрока
+class GenericPlayer : public Hand
+{
+    friend std::ostream& operator<<(std::ostream& os, const GenericPlayer& aGenericPlayer);
+
 public:
-	GenericPlayer(const std::string& name = ""); 
-	virtual ~GenericPlayer();
-	virtual bool isHitting() const = 0; //Указывает, нужна ли игроку еще одна карта. Чистая виртуальная функция
-	bool isBoosted() const; // Указывает, что у игрока перебор
-	void Bust() const; //Объявляет, что у игрока перебор
+    GenericPlayer(const std::string& name = "");
+
+    virtual ~GenericPlayer();
+
+    // показывает, хочет ли игрок продолжать брать карты
+    // Для класса GenericPlayer функция не имеет своей реализации,
+    // т.к. для игрока и дилера это будут разные функции
+    virtual bool IsHitting() const = 0;
+
+    // возвращает значение, если у игрока перебор -
+    // сумму очков большую 21
+    // данная функция не виртуальная, т.к. имеет одинаковую реализацию
+    // для игрока и дилера
+    bool IsBusted() const;
+
+    // объявляет, что игрок имеет перебор
+    // функция одинакова как для игрока, так и для дилера
+    void Bust() const;
+
+protected:
+    std::string m_Name;
 };
 
+GenericPlayer::GenericPlayer(const std::string& name) :m_Name(name){}
+GenericPlayer::~GenericPlayer(){}
 
-GenericPlayer::GenericPlayer(const std::string& name) : m_Name(name) {}
-GenericPlayer::~GenericPlayer() {}
-
-bool GenericPlayer::isBoosted() const {
-	return (GetTotal() > 21);
+bool GenericPlayer::IsBusted() const
+{
+    return (GetTotal() > 21);
 }
 
-void GenericPlayer::Bust() const{
-	std::cout << m_Name << " busts. \n";
+void GenericPlayer::Bust() const
+{
+    std::cout << m_Name << " busts.\n";
 }
 
+std::ostream& operator<<(std::ostream& os, const GenericPlayer& aGenericPlayer)
+{
+    os << aGenericPlayer.m_Name << ":\t";
+
+    std::vector<Card*>::const_iterator pCard;
+    if (!aGenericPlayer.m_Cards.empty())
+    {
+        for (pCard = aGenericPlayer.m_Cards.begin();
+            pCard != aGenericPlayer.m_Cards.end();
+            ++pCard)
+        {
+            os << *(*pCard) << "\t";
+        }
+
+
+        if (aGenericPlayer.GetTotal() != 0)
+        {
+            std::cout << "(" << aGenericPlayer.GetTotal() << ")";
+        }
+    }
+    else
+    {
+        os << "<empty>";
+    }
+
+    return os;
+}
